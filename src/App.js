dcb7a9eb3d97b2d67ffedbc0003cac886ddb4448
huanidz <huannguyena2@gmail.com>
2022-07-09 07:56:08 +0700

    work 7

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import AdministrationPage from './components/AdministrationPage';
 import LessonPage from './components/LessonPage';
 import HomePage from './components/HomePage';
 import NotFound from './pages/NotFound';
-import ExamPage from './components/ExamPage';
 import CoursePage from './components/CoursePage';
 import LoginPage from './components/LoginPage';
 import Index from './components/groupChat/Index'
+import SignUpPage from './components/SignUpPage';
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
 
           <Route path='/admin' element={<AdministrationPage/>} />
           <Route path='/login' element={localStorage.getItem("accessUserId") ? <HomePage/> : <LoginPage/>}/>
+          <Route path='/signup' element={localStorage.getItem("accessUserId") ? <HomePage/> : <SignUpPage/>}/>
           <Route path="/course/:courseid" element={<CoursePage/>} />
           <Route path='/course/:courseid/lesson/:lessonid' element={localStorage.getItem("accessUserId") ? <LessonPage/> : <LoginPage/>} />
           <Route path="/*" component={<NotFound/>} />
